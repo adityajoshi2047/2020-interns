a98fbfdae4402e5dda6d5d4cf4aa00bb46c1944d
aditya2047 <adityajoshi2047@gmail.com>
2020-07-21 14:42:30 +0530

    Task 1 code

---
+++ task1.py
@@ -0,0 +1,35 @@
+import json
+import matplotlib
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt  
+from datetime import datetime
+
+with open("data.json", "r") as read_it: 
+     data = json.load(read_it) 
+
+DATA = []
+
+for k in sorted(data['rates'].keys()):
+    d = []
+    d.append(k)
+    d.append(data['rates'][k]['INR'])
+    DATA.append(d)
+
+exrates = pd.DataFrame(DATA)
+
+fig = plt.figure(figsize=(20,6))
+
+ax1 = fig.add_axes([0, 0, 1, 1])
+
+ax1.set_title('EUR to INR')
+
+ax1.plot(exrates[0],
+         exrates[1],
+         color='blue', label='exchange rate')
+
+ax1.set_xlabel('Date')
+ax1.set_ylabel('value')
+ax1.legend()
+ax1.xaxis.set_major_locator(plt.MaxNLocator(20))
+plt.show()
